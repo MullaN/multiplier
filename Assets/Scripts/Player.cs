c3e01381d5f457fb0563686d88df7fa944d410a4
MullaN <admullan@gmail.com>
2021-03-04 17:52:50 -0800

    initial setup and added player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = .1f;
+    // Start is called before the first frame update
+
+    Rigidbody2D player;
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement(){
+        Vector2 newVelocity = new Vector2(0.0f, 0.0f);
+        if (Input.GetKey(KeyCode.W)){
+            newVelocity.y += moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.S)){
+            newVelocity.y -= moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.D)){
+            newVelocity.x += moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.A)){
+            newVelocity.x -= moveSpeed;
+        }
+        player.velocity = newVelocity;
+    }
+}
