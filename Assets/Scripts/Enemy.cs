400524f547de2eb8418ede408d9c3b2a2508729b
MullaN <admullan@gmail.com>
2021-03-05 09:50:23 -0800

    added enemies and spawner

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float startSpeed = 0.1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, FindObjectOfType<Player>().transform.position, startSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] float spawnTime = 6f;
+
+    private Vector2 spawnPosition;
+
+    private int corner = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    // Update is called once per frame
+    void Spawn()
+    {
+        spawnPosition.x = 9f;
+        spawnPosition.y = 4f;
+
+        if (corner < 2){
+            spawnPosition.x *= -1;
+        }
+
+        if (corner % 2 > 0){
+            spawnPosition.y *= -1;
+        }
+
+        Instantiate(FindObjectOfType<Enemy>(), spawnPosition, Quaternion.identity);
+
+        if (corner == 3) {
+            corner = 0;
+        } else {
+            corner++;
+        }
+    }
+}
