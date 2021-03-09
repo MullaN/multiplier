02a8d6e553b29e1b742399980981a51c154bd33d
MullaN <admullan@gmail.com>
2021-03-09 10:19:12 -0800

    added explosion to multiplier objects

---
+++ Assets/Scripts/TimesBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimesBehaviour : MonoBehaviour
+{
+    [SerializeField] Explosion explosionObject;
+    [SerializeField] int multiplicationAmount;
+    Explosion explosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 9f);
+    }
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.gameObject.name.Contains("Player")){
+            explosion = Instantiate(explosionObject, transform.position, Quaternion.identity);
+            explosion.multiplier = multiplicationAmount;
+            Destroy(gameObject);
+        }
+    }
+}
