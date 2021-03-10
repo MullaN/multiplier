3260a409cb1a0fba0dd7cab977650a782e8c7e47
MullaN <admullan@gmail.com>
2021-03-10 11:48:01 -0800

    created start screen

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadGameScreen(){
+        SceneManager.LoadScene("MainGame");
+    }
+
+    public void LoadStartScreen(){
+        SceneManager.LoadScene("StartScreen");
+    }
+}
