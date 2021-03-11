f7a7ff0d4cc1398dce182dc4b3ce763f0cd60822
MullaN <admullan@gmail.com>
2021-03-11 10:04:25 -0800

    added credits and controls to main screen

---
+++ Assets/Scripts/CreditsControls.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CreditsControls : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI buttonText;
+    [SerializeField] TextMeshProUGUI displayText;
+
+    private string currentButtonText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentButtonText = "CONTROLS";
+    }
+
+    public void TextChange(){
+        if(currentButtonText == "CREDITS"){
+            displayText.text = @"Credits:
+Created by Andrew Mullan
+
+Music by Eric Matyas
+www.soundimage.org
+
+Sounds from freesound:
+Pickup_02.wav by LittleRobotSoundFactory
+Explosion_03.wav by LittleRobotSoundFactory";
+            currentButtonText = "CONTROLS";
+            buttonText.text = "CONTROLS";
+        } else {
+            displayText.text = @"W: Move up
+S: Move down
+A: Move left
+D: Move right
+
+Avoid red circle enemies
+
+Run into x1,x2,x3 to create an explosion to kill red enemies";
+            currentButtonText = "CREDITS";
+            buttonText.text = "CREDITS";
+        }
+    }
+}
